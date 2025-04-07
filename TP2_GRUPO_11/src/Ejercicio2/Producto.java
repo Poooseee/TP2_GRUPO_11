22984599a949cdacedcaa920603220d6f434a3da
liocoma <lionelcorradini@gmail.com>
2025-04-07 19:02:00 -0300

    Complete clase producto

--- TP2_GRUPO_11/src/Ejercicio2/Producto.java
+++ TP2_GRUPO_11/src/Ejercicio2/Producto.java
@@ -3,6 +3,38 @@ package Ejercicio2;
 import java.util.Date;
 
 public class Producto {
+	
+	//Atributos
 	private Date fechaCaducidad;
 	private int numLote;
+	
+	//Constructor con Parametros
+	public Producto(Date fechaCaducidad, int lote) {
+		this.fechaCaducidad = fechaCaducidad;
+		this.numLote = lote;
+	}
+	
+	
+	//Constructor Vacio
+	public Producto() {};
+	
+	//getter y setters
+	public Date getFechaCaducidad() {
+		return fechaCaducidad;
+	}
+	public void setFechaCaducidad(Date fechaCaducidad) {
+		this.fechaCaducidad = fechaCaducidad;
+	}
+	public int getnumLote() {
+		return numLote;
+	}
+	public void setNumLote(int lote) {
+		numLote = lote;
+	}
+	
+	//metodo toString para mostrar
+	@Override
+    public String toString() {
+        return "Fecha de Caducidad: " + fechaCaducidad + ", Lote: " + numLote;
+    }
 }
