9c92a5a1feefb50a3b46d5ce4dbb31961bba37b3
Jano-Guerra11 <janoguerra04@gmail.com>
2025-04-08 12:46:18 -0300

    aporte 1 ejs 2y 3

--- TP2_GRUPO_11/src/Ejercicio2/productosFrescos.java
+++ TP2_GRUPO_11/src/Ejercicio2/productosFrescos.java
@@ -1,5 +1,26 @@
 package Ejercicio2;
+import java.util.Date;
 
 public class productosFrescos extends Producto {
-
+  private Date fechaEnvasado;
+  private String paisOrigen;
+  
+  // CONSTRUCTORES
+  public productosFrescos() {
+	  super();
+	  fechaEnvasado = new Date(); // fecha actual
+	  paisOrigen = "sin especificar";
+  }
+  public productosFrescos(Date fechaCaducidad, int lote,Date fechaEnvasado,String paisOrigen) {
+	  super(fechaCaducidad,lote);
+	  this.fechaEnvasado = fechaEnvasado;
+	  this.paisOrigen = paisOrigen;
+  }
+@Override
+public String toString() {
+	return "producto Fresco: FECHA CADUCIDAD: "+this.getFechaCaducidad()+", LOTE: "
+			+this.getnumLote()+", FECHA ENVASADO: "+fechaEnvasado+", PAIS: "+paisOrigen;
+}
+  
+  
 }
--- TP2_GRUPO_11/src/Ejercicio3/edificioOficinas.java
+++ TP2_GRUPO_11/src/Ejercicio3/edificioOficinas.java
@@ -1,5 +1,18 @@
 package Ejercicio3;
 
 public class edificioOficinas extends Edificio {
+	private int numeroOficinas;
+	private double superficie;
 	
+	public edificioOficinas() {
+		numeroOficinas = 0;
+		superficie = 0.0;
+	}
+	public edificioOficinas(int numOficinas,double superficie) {
+		this.numeroOficinas = numOficinas;
+		this.superficie = superficie;
+	}
+public String toString() {
+	return "edificio de oficinas: superficie: "+superficie+", numero oficinas: "+numeroOficinas;
+}
 }
